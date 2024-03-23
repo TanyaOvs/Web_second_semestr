d5de0dc71c383e14e9510a194620927ebf2e8f61
TanyaOvs <144003852+TanyaOvs@users.noreply.github.com>
2024-03-23 14:54:07 +0300

    Update index.php

--- 3/index.php
+++ 3/index.php
@@ -12,6 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $gender = isset($_POST['gender']) ? $_POST['gender'] : '';
     $bio = $_POST['bio'];
     $check = isset($_POST['check']) ? 1 : 0;
+    $languages = isset($_POST['language']) ? $_POST['language'] : '';
 
     // Проверка корректности заполнения полей
 	echo "<div class='error-message-container'>";
@@ -36,10 +37,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if (empty($gender)) {
         $errors[] = "Пожалуйста, выберите пол.";
+    } elseif ($gender !== 'male' && $gender !== 'female') {
+          $errors[] = "Некорретктное значение пола.";
     }
 
-    if (!preg_match("/^[a-zA-Zа-яА-Я.,! ]+$/u", $bio)) {
+    if (!preg_match("/^[a-zA-Zа-яА-Я.,! ]*$/u", $bio)) {
         $errors[] = "Поле Биография не может содержать специальные символы!";
+    }
+
+    // Список допустимых языков программирования
+    if(empty($languages)){
+        $errors[] = "Вы не выбрали Любимый язык программирования!";
+    }
+    else{
+        $valid_languages = array("Pascal", "C", "C++", "JavaScript", "PHP", "Python", "Java", "Haskell", "Clojure", "Prolog", "Scala");
+        foreach ($languages as $p_language) {
+            if (!in_array($p_language, $valid_languages)) {
+                $errors[] = "Некорректные данные в 'Любимый язык программирования'!";
+                break;
+            }
+        }
     }
 
     if (!empty($errors)) {
@@ -48,17 +65,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
     }
     else {
-        $stmt = $db->prepare("INSERT INTO Applications (FIO, Phone, Email, Birthdate, Gender, Contract, Bio) VALUES (?, ?, ?, ?, ?, ?, ?)");
-        if($stmt->execute([$name, $phone, $email, $birthdate, $gender, $check, $bio])){
+        $application = $db->prepare("INSERT INTO Applications (FIO, Phone, Email, Birthdate, Gender, Contract, Bio) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        if($application->execute([$name, $phone, $email, $birthdate, $gender, $check, $bio])){
             $last_id = $db->lastInsertId();
-            foreach ($_POST['language'] as $language) {
-                $stmt = $db->prepare("SELECT ID FROM Programming_Languages WHERE ProgrammingLanguage = ?");
-                $stmt->execute([$language]);
-                $programming_language_id = $stmt->fetchColumn();
 
-                $stmt = $db->prepare("INSERT INTO Application_Ability (ApplicationID, ProgrammingLanguageID) VALUES (?, ?)");
+            //Подготовка данных
+            $stmt_language = $db->prepare("SELECT ID FROM Programming_Languages WHERE ProgrammingLanguage = ?");
+            $stmt = $db->prepare("INSERT INTO Application_Ability (ApplicationID, ProgrammingLanguageID) VALUES (?, ?)");
+
+            foreach ($languages as $language) {
+                $stmt_language->execute([$language]);
+                $programming_language_id = $stmt_language->fetchColumn();
                 $stmt->execute([$last_id, $programming_language_id]);
-                }
+            }
+
         header('Location: form.php?actionsCompleted=1');
         exit();
         }
