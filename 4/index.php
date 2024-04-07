4fe405452f8f3e47a71f2654a1eceaa4d9c364b4
TanyaOvs <144003852+TanyaOvs@users.noreply.github.com>
2024-04-07 21:14:12 +0300

    Update index.php

--- 4/index.php
+++ 4/index.php
@@ -89,8 +89,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET'){
     include('form.php');
 }
 else {
-    $user = 'u67310';
-    $pass = '8200920';
+    $user = '*****';
+    $pass = '*****';
     $db = new PDO('mysql:host=localhost;dbname=u67310', $user, $pass);
 
     //Данные из формы
